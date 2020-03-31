5ba646e91ce176143c53757ee3bb562adcbd6013
Yilda Boukhtouchen <48527851+yildab@users.noreply.github.com>
2020-03-30 22:01:57 -0400

    Delete rnn.sh

--- RNN/rnn.sh
+++
@@ -1,5 +0,0 @@
-python3 rnn_class.py -q 4 -lr 0.00001
-python3 rnn_class.py -q 5 -lr 0.00001
-python3 rnn_class.py -q 2 -lr 0.00001
-python3 rnn_class.py -q 3 -lr 0.00001
-python3 rnn_class.py -q 6 -lr 0.00001
